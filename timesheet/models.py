88f06cb0b9710a0f2ca75f77d3f7ad11dca8bbf0
Tor Solli-Nowlan <tor.solli-nowlan@medisin.uio.no>
2020-10-22 20:10:48 +0200

    track flexed days

--- timesheet/models.py
+++ timesheet/models.py
@@ -1,5 +1,4 @@
-from sqlalchemy import Column, Date, Time
-from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Date, Time, Boolean
 
 from .db import Base
 from .util import Log
@@ -11,6 +10,7 @@ class Timesheet(Base):
     date = Column(Date, primary_key=True, unique=True, index=True)
     clock_in = Column(Time)
     clock_out = Column(Time)
+    is_flex = Column(Boolean, default=False)
 
     def __repr__(self) -> str:
         return f"<Timesheet date={self.date} clock_in={self.clock_in} clock_out={self.clock_out}"

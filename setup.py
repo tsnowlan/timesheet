9a165f972afbaf1dae2b9d9bb25f436c0419ae4c
tsnowlan <tsnowlan@gmail.com>
2020-11-14 19:28:57 +0100

    expand optional deps

--- setup.py
+++ setup.py
@@ -7,9 +7,10 @@ setup(
         "sqlalchemy~=1.3",
         "click~=7.1",
     ],
-    extras_require={"dev": ["sqlalchemy-stubs==0.3", "black~=20.7"]},
-    packages=["timesheet"],
-    entry_points={
-        "console_scripts": ["timesheet = timesheet:main", "clock = timesheet:clock"]
+    extras_require={
+        "dev": ["sqlalchemy-stubs==0.3", "black~=20.8b1", "flake8~=3.8.4", "pylint~=2.6.0"],
+        "format": ["pyyaml~=5.3.1", "toml~=0.10.2"],
     },
+    packages=["timesheet"],
+    entry_points={"console_scripts": ["timesheet = timesheet:main", "clock = timesheet:clock"]},
 )

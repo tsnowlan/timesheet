da2c4c9ac051175bea6541cf442009a8b7b473e9
Tor Solli-Nowlan <tsnowlan@gmail.com>
2020-11-30 10:54:04 +0100

    improve setup reqs

--- setup.py
+++ setup.py
@@ -4,12 +4,25 @@ setup(
     name="timesheet",
     version="v1.0.0",
     install_requires=[
-        "sqlalchemy~=1.3",
-        "click~=7.1",
+        "sqlalchemy>=1.3",
+        "click>=7.1",
     ],
-    extras_require={"dev": ["sqlalchemy-stubs==0.3", "black~=20.7"]},
+    python_requires=">=3.9",
+    extras_require={
+        "dev": [
+            "sqlalchemy-stubs>=0.3",
+            "black>=20.7",
+        ],
+        "full": [
+            "toml",
+            "PyYAML>=5.3",
+        ],
+    },
     packages=["timesheet"],
     entry_points={
-        "console_scripts": ["timesheet = timesheet:main", "clock = timesheet:clock"]
+        "console_scripts": [
+            "timesheet = timesheet:main",
+            "clock = timesheet:clock",
+        ]
     },
 )
